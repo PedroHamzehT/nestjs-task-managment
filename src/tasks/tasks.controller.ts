9a060a3798936c365d826a8ad1e53e163f44ea24
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-26 19:19:53 -0300

    add enum validation with the new update task status dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,8 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { GetTasksFilterDto } from './dtos/get-tasks-filter.dto';
-import { Task, TaskStatus } from './task.model';
+import { UpdateTaskStatusDto } from './dtos/update-task-status.dto';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -44,8 +45,10 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatus: UpdateTaskStatusDto,
   ): Task {
+    const { status } = updateTaskStatus;
+
     return this.tasksService.updateTaskStatus(id, status);
   }
 }

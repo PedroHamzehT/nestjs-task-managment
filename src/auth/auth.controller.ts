807cfa683f63350e1d11735cb8c1c9fdce455d65
Pedro <pedrohamzeh@gmail.com>
2022-02-10 08:14:27 -0300

    add sign in endpoint and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,8 +6,13 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post()
+  @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
